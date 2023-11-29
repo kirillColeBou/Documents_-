e51cabf8a426c9e173d5ef4cd4a4e9101d65b2a6
student-a210 <student-a210@PC-A210-5>
2023-11-29 14:07:38 +0500

    Реализованные интерфейсы. Тепляков

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Тепляков.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Documents_Тепляков.Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}

469b2dac88577a129fd0a2967e4e5ab71ede3247
student-a210 <student-a210@PC-A210-5>
2023-11-29 14:50:06 +0500

    Реализованные классы. Тепляков

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.OleDb;
+
+namespace Documents_Тепляков.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string Path = @"C:\Users\student-a210\Desktop\PR_21.accdb";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            oleDbConnection.Open();
+            return oleDbConnection;
+        }
+        public static OleDbDataReader Query(string Query, OleDbConnection Connection)
+        {
+            return new OleDbCommand(Query, Connection).ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
